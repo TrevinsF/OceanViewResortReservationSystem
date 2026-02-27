0c150f5261604f3b941e28c50d29d9a31a9e8669
TrevinsF <trevinsfdo123456@gmail.com>
2026-02-28 03:20:19 +0530

    Added DBConnection

---
+++ src/main/java/com/oceanview/util/DBConnection.java
@@ -0,0 +1,34 @@
+package com.oceanview.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String DEFAULT_URL = "jdbc:mysql://localhost:3306/oceanview_db?useSSL=false";
+    private static final String JDBC_USERNAME = "root";
+    private static final String JDBC_PASSWORD = "1234";
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        // This is the missing link! It checks if a test URL was provided by the JUnit test.
+        String url = System.getProperty("test.db.url", DEFAULT_URL);
+
+        if (url.contains("mysql")) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } else if (url.contains("h2")) {
+            Class.forName("org.h2.Driver");
+        }
+
+        return DriverManager.getConnection(url, JDBC_USERNAME, JDBC_PASSWORD);
+    }
+
+    public static void closeResources(AutoCloseable connection, AutoCloseable statement, AutoCloseable resultSet) {
+        try {
+            if (resultSet != null) resultSet.close();
+            if (statement != null) statement.close();
+            if (connection != null) connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
